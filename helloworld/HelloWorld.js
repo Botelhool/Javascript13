ac4f29fc271471c01e48067e5b22b8cbe145ed71
botelhool <leo.obotelho@outlook.com>
2026-01-26 11:32:31 -0300

    Hello world js

---
+++ helloworld/HelloWorld.js
@@ -0,0 +1,7 @@
+const leia = require("readline-sync");
+
+let numero = leia.questionInt("Digite um numero: ");
+let numero2= leia.questionInt("Digite outro numero: ")
+
+let soma = numero + numero
+console.log(soma)
